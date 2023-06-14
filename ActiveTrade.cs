e27aaef1035c36cc4157c3a41ebba3b7207ab512
Mitya83p <127844357+Mitya83p@users.noreply.github.com>
2023-06-14 11:42:06 +0300

    Add files via upload
    Активный трейд

---
+++ ActiveTrade.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PrismaBoy
+{
+    enum Direction
+    {
+        Buy,
+        Sell
+    }
+
+    [Serializable]
+
+    class ActiveTrade
+    {
+        public long ProfitOrderTransactionId { get; set; }
+        public long ProfitOrderId { get; set; }
+
+        public long StopLossOrderTransactionId { get; set; }
+        public long StopLossOrderId { get; set; }
+
+        public bool IsStopOrderPlaced;
+
+
+        public long Id;
+        public string Security;
+        public Direction Direction;
+        public decimal Price;
+        public decimal Volume;
+        public DateTime Time;
+        public decimal StopPrice;
+
+        /// <summary>
+        /// Имя заявки которая привела к трейду
+        /// </summary>
+        public string OrderName;
+
+        /// <summary>
+        /// Конструктор класса ActiveTrade
+        /// </summary>
+        public ActiveTrade(long id, string security, Direction direction, decimal price, decimal volume, DateTime time, decimal stopPrice, string orderName)
+        {
+            Id = id;
+            Security = security;
+            Direction = direction;
+            Price = price;
+            Volume = volume;
+            Time = time;
+            IsStopOrderPlaced = false;
+            StopPrice = stopPrice;
+            OrderName = orderName;
+        }
+    }
+}
